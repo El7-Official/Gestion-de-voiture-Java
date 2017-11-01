3e983da0f8a90089030e459a455da793e8cdde29
AyoubTeallix <elhassaniayoub00@gmail.com>
2017-11-01 01:59:28 +0000

    Ajout d'une variable de classe prixSigne qui prend le signe du prix ($,dh, ...), Instanciation de la liste options ainsi que la variable nom dans le constructeur, Modification de la fonction toString()

--- src/VehiculeMarque/Vehicule.java
+++ src/VehiculeMarque/Vehicule.java
@@ -1,19 +1,30 @@
 package VehiculeMarque;
+
+import java.util.ArrayList;
 import java.util.List;
 import VehiculeMoteurs.Moteur;
 import VehiculeOptions.Option;
 
 public abstract class Vehicule {
+	public static char prixSigne = '€';
 	private double prix;
 	private String nom;
 	private List<Option> options;
-	private Marque nomMarque;
+	protected Marque nomMarque;
 	private Moteur motVehicule;
 	
-	public String toString() {
-		return "Juste implémentation";
+	public Vehicule() {
+		//Instanciation de la liste des options
+		this.options = new ArrayList<>();
+		//Instanciation du variable nom qui prend le nom de la classe associé à l'instance d'objet
+		this.nom = this.getClass().getName();
 	}
 	
+	public String toString() {
+		return "+ Voiture "+this.nomMarque+" : "+this.nom+" "+this.motVehicule.toString()+" ["+this.options.toString()+"]";
+	}
+	
+	//Spécifier les options à ajouter à une liste d'options du voitures
 	public void addOption(Option opt) {
 		this.options.add(opt);
 	}
