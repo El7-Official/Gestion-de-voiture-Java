fe30d2d469e473949bbaaf4b402dc0c35d80f09a
AyoubTeallix <elhassaniayoub00@gmail.com>
2017-11-01 02:29:30 +0000

    Ajout d'une constructer pour initialiser la liste des voitures, implémentation de la fonction addVoiture(Vehicule voiture), Ajout de la fonction getVoiture pour afficher la liste des voitures existe dans la liste, Changement au niveau de la fonction toString()

--- src/Garage.java
+++ src/Garage.java
@@ -1,14 +1,30 @@
 import VehiculeMarque.Vehicule;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Garage {
 	List<Vehicule> voitures;
 	
-	public String toString() {
-		return "Juste implémentation";
+	
+	public Garage() {
+		//Instanciation de la liste Voitures
+		voitures = new ArrayList<>();
 	}
 	
-	public void add(Vehicule voit) {
-		
+	public String toString() {
+		System.out.println("*******************************************\n"+
+						   "*   Garage du Voiture d'Ayoub El hassani  *\n"+
+						   "*******************************************");
+		return getVoiture();
+	}
+	
+	public void addVoiture(Vehicule voit) {
+		voitures.add(voit);
+	}
+	
+	private String getVoiture() {
+		for(Vehicule voiture : voitures)
+			System.out.println(" "+voiture.toString());
+		return "";
 	}
 }
