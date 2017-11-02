e8780b3ff8deb54b6ac529462507f6d04719836c
AyoubTeallix <elhassaniayoub00@gmail.com>
2017-11-02 01:08:07 +0000

    final implementations des méthodes addVoiture() toString() et suppression de la methode getVoiture(), cette classe maintenant travaille avec des fichiers en ajoutant des voitures à un fichier

--- src/Garage.java
+++ src/Garage.java
@@ -1,30 +1,48 @@
-import VehiculeMarque.Vehicule;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import VehiculeMarque.Vehicule;
 
 public class Garage {
+	FileInputStream fis;
+	FileOutputStream fos;
+	ObjectInputStream ois;
+	ObjectOutputStream oos;
 	List<Vehicule> voitures;
 	
-	
 	public Garage() {
-		//Instanciation de la liste Voitures
-		voitures = new ArrayList<>();
+		voitures = new ArrayList<Vehicule>();
 	}
 	
 	public String toString() {
-		System.out.println("*******************************************\n"+
-						   "*   Garage du Voiture d'Ayoub El hassani  *\n"+
-						   "*******************************************");
-		return getVoiture();
-	}
-	
-	public void addVoiture(Vehicule voit) {
-		voitures.add(voit);
-	}
-	
-	private String getVoiture() {
-		for(Vehicule voiture : voitures)
-			System.out.println(" "+voiture.toString());
+		try {
+			fis = new FileInputStream("garage.txt");
+			ois = new ObjectInputStream(fis);
+			voitures = ((List<Vehicule>)ois.readObject());
+			for(Vehicule voit : voitures)
+				System.out.println(voit);
+		} catch (IOException | ClassNotFoundException e) {
+			try {
+				throw new Exception("Aucune voiture sauvegardé !");
+			} catch (Exception e1) {
+				return e1.getMessage();
+			}
+		}
 		return "";
+	}
+	
+	public void addVoiture(Vehicule voit){
+		voitures.add(voit); 
+		try {
+			fos = new FileOutputStream("garage.txt");
+			oos = new ObjectOutputStream(fos);	
+			oos.writeObject(voitures);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
